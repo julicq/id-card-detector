e456e04ccb320030ea08ecd8a93b6fbe34a355e6
Yulian Pavlov <yulianpavlov@Yulians-MacBook-Pro.local>
2021-11-05 16:02:53 +0400

    code fix for tfv1.compatibility

--- id_card_detection_image.py
+++ id_card_detection_image.py
@@ -1,4 +1,3 @@
-
 # Import packages
 import os
 import cv2
@@ -24,13 +23,13 @@ CWD_PATH = os.getcwd()
 
 # Path to frozen detection graph .pb file, which contains the model that is used
 # for object detection.
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
 
 # Path to label map file
-PATH_TO_LABELS = os.path.join(CWD_PATH,'data','labelmap.pbtxt')
+PATH_TO_LABELS = os.path.join(CWD_PATH, 'data', 'labelmap.pbtxt')
 
 # Path to image
-PATH_TO_IMAGE = os.path.join(CWD_PATH,IMAGE_NAME)
+PATH_TO_IMAGE = os.path.join(CWD_PATH, IMAGE_NAME)
 
 # Number of classes the object detector can identify
 NUM_CLASSES = 1
@@ -41,19 +40,20 @@ NUM_CLASSES = 1
 # Here we use internal utility functions, but anything that returns a
 # dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 # Load the Tensorflow model into memory.
-detection_graph = tf.Graph()
+detection_graph = tf.compat.v1.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
 
-    sess = tf.Session(graph=detection_graph)
+    sess = tf.compat.v1.Session(graph=detection_graph)
 
 # Define input and output tensors (i.e. data) for the object detection classifier
 
@@ -101,12 +101,13 @@ im_width, im_height = shape[1], shape[0]
 (left, right, top, bottom) = (xmin * im_width, xmax * im_width, ymin * im_height, ymax * im_height)
 
 # Using Image to crop and save the extracted copied image
-im = Image.open(image_path)
-im.crop((left, top, right, bottom)).save(output_path, quality=95)
+im = Image.open(PATH_TO_IMAGE)
+out_file = 'test_images/test.png'
+im.crop((left, top, right, bottom)).save(out_file, quality=95)
 
 cv2.imshow('ID-CARD-DETECTOR : ', image)
 
-image_cropped = cv2.imread(output_path)
+image_cropped = cv2.imread(out_file)
 cv2.imshow("ID-CARD-CROPPED : ", image_cropped)
 
 # All the results have been drawn on image. Now display the image.
